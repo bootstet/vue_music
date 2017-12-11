55f930b22712e7158f00e0506cc7aec8415488ba
bootstet <bootstet@163.com>
2017-12-11 23:36:17 +0800

    vue_music

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      排行
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped lang="scss" lef="stylesheet/scss">
+
+</style>
+
+
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      推荐页
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped lang="scss" lef="stylesheet/scss">
+
+</style>
+
+
